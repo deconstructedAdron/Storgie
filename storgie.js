58649c6d8779c44cee573a9ddc504c67572f1b60
Adron Hall <adronhall@gmail.com>
2013-11-26 15:40:12 -0800

    Adding the storgie.js file before blogging it.

--- storgie.js
+++ storgie.js
@@ -1,12 +1,22 @@
 var restify = require('restify');
 
-function respond(req, res, next) {
+function send(req, res, next) {
     res.send('hello ' + req.params.name);
+    return next();
 }
 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.post('/hello', function create(req, res, next) {
+    res.send(201, Math.random().toString(36).substr(3, 8));
+    return next();
+});
+server.put('/hello', send);
+server.get('/hello/:name', send);
+server.head('/hello/:name', send);
+server.del('hello/:name', function rm(req, res, next) {
+    res.send(204);
+    return next();
+});
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
