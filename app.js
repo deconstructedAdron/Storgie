11a5bfeabc5bf9524b98211c5ff4c81949cdcd50
Adron Hall <adronhall@gmail.com>
2013-11-30 00:34:09 -0800

    Added favicon to app.js.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -11,7 +10,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('name', 'storgie');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
