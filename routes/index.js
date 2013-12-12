da5a8a12d009be979deaacc89adfe9cf362ecfd4
Adron Hall <adronhall@gmail.com>
2013-12-12 10:41:11 -0800

    Data generation. Added a test for keyios and keybrowser plus data. Still just testing - not even close to final code for this.

--- routes/index.js
+++ routes/index.js
@@ -54,6 +54,33 @@ exports.ident_create = function (req, res) {
         });
 };
 
+function gen_ios_ident(matched) {
+    var data_iOS = new Object();
+    data_iOS.AdId = chance.guid();
+    data_iOS.VendorId = chance.guid();
+    data_iOS.UUID = chance.guid();
+    data_iOS.DevId = chance.guid();
+    data_iOS.IP = chance.guid();
+    data_iOS.WebId = chance.guid();
+    data_iOS.Cookie = chance.guid();
+    data_iOS.AppId = chance.guid();
+    data_iOS.HashEmail = chance.guid();
+    data_iOS.HashIdent = matched;
+    return data_iOS;
+}
+
+function gen_browser_ident(matched) {
+    var data_browser = new Object();
+    data_browser.HashId = matched;
+    data_browser.WebId = chance.guid();
+    data_browser.AppId = chance.guid();
+    data_browser.Cookie = chance.guid();
+    data_browser.HashedItem1 = chance.guid();
+    data_browser.HashedItem2 = chance.guid();
+    data_browser.HashedItem3 = chance.guid();
+    return data_browser;
+}
+
 exports.scenario_create = function (req, res) {
     if (!req.body.hasOwnProperty('rowgen')) {
         res.statusCode = 400;
@@ -63,41 +90,28 @@ exports.scenario_create = function (req, res) {
     // Create
     var matched = chance.guid();
 
-    var data_iOS = new Object();
-    data_iOS.AdId = chance.guid();
-    data_iOS.VendorId = chance.guid();
-    data_iOS.UUID = matched;
-    data_iOS.DevId = chance.guid();
-    data_iOS.IP = chance.guid();
-    data_iOS.WebId = chance.guid();
-    data_iOS.Cookie = chance.guid();
-    data_iOS.AppId = chance.guid();
-    data_iOS.HashEmail = chance.guid();
-    data_iOS.HashIdent = chance.guid();
+    var data_iOS = gen_ios_ident(matched);
+    var data_browser = gen_browser_ident(matched);
 
-    var data_Android = new Object();
-    data_Android.
+    var value_data_ios = JSON.stringify(data_iOS);
+    var value_data_browser = JSON.stringify(data_browser)
 
+    var keyios = '186A91F7-45D7-4088-A644-306C6937FB5E';
+    var keybrowser = '126A91F7-45D7-4088-A644-306C6937FB5E';
 
-        var
-    valueData = JSON.stringify(data_iOS);
+    function store_ident(deconstructed_key, value_data) {
 
-    var keyValue = new Object();
-    keyValue.key = 'test'; //chance.guid();
-    keyValue.value = valueData;
-
-    function extracted() {
-        orchestrator.put(collection, keyValue.key, keyValue.value)
+        orchestrator.put(collection, deconstructed_key, value_data)
             .then(function (result) {
-                var returnThis = keyValue.key + ' written.';
+                var returnThis = deconstructed_key + ' written.';
                 console.log(returnThis);
                 res.send(returnThis);
-
             })
             .fail(function (err) {
                 console.log('An error ' + err);
             });
     }
 
-    extracted();
+    store_ident(keyios, value_data_ios);
+    store_ident(keybrowser, value_data_browser);
 };
