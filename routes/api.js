3a47cfaf75221173085fad4ee951b09fa60d644b
Adron Hall <adronhall@gmail.com>
2013-12-24 07:59:29 -0800

    Fake data piece one.

--- routes/api.js
+++ routes/api.js
@@ -22,10 +22,11 @@ exports.storgie_stat = function (req, res) {
 
 exports.ident_by_id = function (req, res) {
 
+    var result = extraction_fake();
 
 
     console.log("Retrieved");
-    return res.send(req);
+    return res.send(result);
 };
 
 exports.convergence = function (req, res) {
@@ -54,3 +55,19 @@ exports.scenario_create = function (req, res) {
 
     res.send(JSON.stringify(answer));
 };
+
+function extraction_fake() {
+    var data_iOS = {
+        "AdId": "B5A877B1-5D67-5101-9B71-FF573539BBCD",
+        "VendorId": "F15DE2B3-17E0-5129-B37F-1B126E3FA8AA",
+        "UUID": "F9D8ABB6-1B50-59F4-8312-EADA5EDA3DFF",
+        "DevId": "AA632158-E04A-511C-90A9-77B11850FA4C",
+        "IP": "789149BB-870C-5531-950B-A2BE6FA09884",
+        "WebId": "4E33A7FA-8C78-5455-871F-C75C52AE5E4E",
+        "Cookie": "6E84E2B2-7796-5D3B-BBA0-162358A79742",
+        "AppId": "394287E9-9ACC-5C5B-82D4-0EF06B827DB8",
+        "HashEmail": "AC5F7FB4-3C3F-54EE-815C-E44461046038",
+        "HashIdent": "ident1"};
+
+    return JSON.stringify(data_iOS);
+}
