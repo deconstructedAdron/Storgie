fefe15991a8c530b8cf97bbd3c2d091bc096a8a4
Adron Hall <adronhall@gmail.com>
2013-12-24 15:51:50 -0800

    Adding android data generator.

---
+++ generators/android.js
@@ -0,0 +1,17 @@
+var Chance = require('chance'),
+    chance = new Chance(),
+    android = exports;
+
+android.android_ident = function () {
+    var data_browser = new Object();
+    data_browser.HashId = chance.guid();
+    data_browser.WebId = chance.guid();
+    data_browser.AppId = chance.guid();
+    data_browser.Cookie = chance.guid();
+    data_browser.HashedItem1 = chance.guid();
+    data_browser.HashedItem2 = chance.guid();
+    data_browser.HashedItem3 = chance.guid();
+    data_browser.HashEmail = chance.guid();
+    data_browser.HashIdent = chance.guid();
+    return data_browser;
+}
