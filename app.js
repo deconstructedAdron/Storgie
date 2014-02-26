1728e17a9671f7eaa93196984caf044797a974c7
Adron Hall <adronhall@gmail.com>
2014-02-26 10:26:09 -0800

    Added passport depednencies for passport & passport-local.

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var express = require ('express');
-var site = require ('./routes/site');
-var api = require ('./routes/api');
-var http = require ('http');
-var path = require ('path');
+var express = require('express');
+var site = require('./routes/site');
+var api = require('./routes/api');
+var http = require('http');
+var path = require('path');
 
-var app = express ();
+// Passport Security
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-// all environments
-app.set ('port', process.env.PORT || 3010);
-app.set ('views', path.join (__dirname, 'views'));
-app.set ('view engine', 'jade');
-app.set ('name', 'storgie');
-app.use (express.favicon (path.join (__dirname, '/public/favicon.ico')));
-app.use (express.logger ('dev'));
-
-app.use (express.json ());
-app.use (express.urlencoded ());
-
-app.use (express.methodOverride ());
-app.use (app.router);
-app.use (express.static (path.join (__dirname, 'public')));
-
+var app = express();
+app.set('port', process.env.PORT || 3010);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('name', 'storgie');
+app.use(express.favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+// Passport
+app.use(passport.initialize());
 
 // development only
-if ('development' == app.get ('env')) {
-    app.use (express.errorHandler ());
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
 }
 
+var Account = require(__dirname + '/models/account');
+
 // storgie server information
-app.get ('/', site.index);
-app.get ('/login', site.login);
-app.get ('/signup', site.signup);
-app.get ('/status', site.status);
+app.get('/', site.index);
+app.get('/login', site.login);
+app.get('/signup', site.signup);
+app.get('/status', site.status);
 
 // storgie api identity ident management.
 app.get('/status', api.storgie_stat);
@@ -39,13 +42,13 @@ app.post('/identity', api.identity_create);
 app.get('/identity/:id', api.identity_by_id);
 
 // storgie api converged data
-app.get ('/convergence', api.convergence);
-app.post ('/converged', api.converged_create);
-app.get ('/converged/:id', api.converged_by_id);
+app.get('/convergence', api.convergence);
+app.post('/converged', api.converged_create);
+app.get('/converged/:id', api.converged_by_id);
 
 // storgie scenario generator
-app.post ('/scenario', api.scenario_create);
+app.post('/scenario', api.scenario_create);
 
-http.createServer (app).listen (app.get ('port'), function () {
-    console.log (app.get ('name') + ' server listening on port ' + app.get ('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log(app.get('name') + ' server listening on port ' + app.get('port'));
 });
