6d6d41138309a1d41d96060e877a1afa3aa447d2
Adron Hall <adronhall@gmail.com>
2014-03-04 15:51:00 -0800

    Got those done... on to the next few.

--- app.js
+++ app.js
@@ -34,26 +34,12 @@ function findByToken(token, fn) {
     return fn(null, null);
 }
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-    findById(id, function (err, user) {
-        done(err, user);
-    });
-});
-
 passport.use(new BearerStrategy({
     },
     function (token, done) {
         // asynchronous validation, for effect...
         process.nextTick(function () {
 
-            // Find the user by token.  If there is no user with the given token, set
-            // the user to `false` to indicate failure.  Otherwise, return the
-            // authenticated `user`.  Note that in a production-ready application, one
-            // would want to validate the token for authenticity.
             findByToken(token, function (err, user) {
                 if (err) {
                     return done(err);
@@ -103,16 +89,31 @@ app.get('/signup', site.signup);
 app.get('/status', site.status);
 app.post('/signup', site.signingup);
 
+// *********************************************************************************************************************
 // storgie api identity ident management.
+// *********************************************************************************************************************
 app.get('/stat', api.storgie_stat);
 // curl localhost:3010/stat
 app.post('/identity', api.identity_create);
 // curl -X POST -H "Content-Type: application/json" -d '{"apikey":"asdasjsdgfjkjhg", "key":"10","value":{"knownid":{"Id":"1","SampleId":"324","EmailId":"blagh@blagh.com"}}}' http://localhost:3010/identity
 app.get('/identity/:id', api.identity_by_id);
 
+// *********************************************************************************************************************
 // storgie api converged data
-app.get('/convergence', api.convergence);
-app.post('/converged', api.converged_create);
+// *********************************************************************************************************************
+
+// curl -v http://localhost:3010/convergence?access_token=123456789
+app.get('/convergence', passport.authenticate('bearer', { session: false }),
+    function (req, res) {
+        api.convergence(req, res);
+    });
+
+// curl -v -X POST -d '{"key":"1","value":"testing"}' http://localhost:3010/converged?access_token=123456789
+app.post('/converged',
+    passport.authenticate('bearer', { session: false }),
+    function (req, res) {
+        api.converged_create(req, res);
+    });
 
 // curl -v -X POST -d '{"key":"1","value":"testing"}' http://localhost:3010/converged/by?access_token=123456789
 // curl -v -X POST -d '{"key":"2","value":"{"knownid":"{"oneid":"1234","emailid":"blagh@blagh.com"}"}"}' http://localhost:3010/converged/by?access_token=123456789
@@ -122,7 +123,10 @@ app.post('/converged/by',
         api.converged_by_id(req, res);
     })
 
+// *********************************************************************************************************************
 // storgie scenario generator
+// *********************************************************************************************************************
+
 // curl -v -X POST -d '{"rowgen":"yes"}' http://localhost:3010/scenario?access_token=123456789
 app.post('/scenario',
     passport.authenticate('bearer', { session: false }),
