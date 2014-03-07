e483086d0da4cfca22754a807ee74825b4538691
Adron Hall <adronhall@gmail.com>
2014-03-06 21:44:30 -0800

    Added ignore note information. Almost have everything in place.

--- tests/generators.js
+++ tests/generators.js
@@ -1,4 +1,5 @@
 var assert = require ("assert")
+var fs = require('fs');
 
 describe ('Array', function () {
     describe ('#indexOf()', function () {
@@ -11,7 +12,6 @@ describe ('Array', function () {
 
 var android_generator = require ('../generators/android');
 var ios_generator = require ('../generators/ios');
-var windows_generator = require ('../generators/windows');
 
 describe ('generator', function () {
     describe ('plural', function () {
@@ -26,5 +26,34 @@ describe ('generator', function () {
         it ('should return multiple generated iOS idents', function () {
             assert.equal (ios_generator.idents_generate (8).length, 8);
         })
+    })
+})
+
+function GetDeconstructedConfig(fileName) {
+    fs.exists(fileName, function (exists) {
+        if (exists) {
+            fs.stat(fileName, function (error, stats) {
+                fs.open(fileName, "r", function (error, fd) {
+                    var buffer = new Buffer(stats.size);
+                    fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+                        var data = buffer.toString("utf8", 0, buffer.length);
+                        console.log(data);
+                        var json = JSON.parse(data);
+                        console.log(json.key);
+                        fs.close(fd);
+                        return json.key;
+                    });
+                });
+            });
+        }
+    });
+}
+
+describe('The configuration file', function () {
+    it('should have a key value set with the database API key.', function () {
+        var file = './config.json';
+        var result = GetDeconstructedConfig(file);
+
+        assert.equal('win!', result);
     })
 })
