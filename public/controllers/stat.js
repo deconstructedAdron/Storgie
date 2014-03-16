aab91b712c8d40a9972b833c0cb386958780dbcb
Adron Hall <adronhall@gmail.com>
2014-03-16 14:26:43 -0700

    Finished implementing the basic Stat test.

--- public/controllers/stat.js
+++ public/controllers/stat.js
@@ -3,12 +3,18 @@
  * Description a simple controller to get the results of the stat API service.
  */
 
-var rootAPI = 'http://api.deconstructed.io/';
+//var rootAPI = 'http://api.deconstructed.io/';
+var rootAPI = 'http://localhost:3010/'
 
-function Hello($scope, $http) {
-    $http.get(rootAPI + 'stat').
+function Stat($scope, $http) {
+    $http({ method: 'GET', url: rootAPI + 'stat?access_token=123456789' }).
         success(function (data) {
-            $scope.greeting = data;
+            $scope.storgie_status = data;
+        }).
+        error(function (data, status, headers, config) {
+            $scope.error = data;
+            $scope.status = status;
+            $scope.headers = headers;
         });
 }
 
