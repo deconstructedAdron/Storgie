13e168a312fa00cc65145d5a2a659bb06903cae4
Adron Hall <adronhall@gmail.com>
2014-03-19 14:13:36 -0700

    Messing with users.

--- accounts/users.js
+++ accounts/users.js
@@ -8,7 +8,8 @@ var users = exports;
 
 users.getUsers = function getUsers() {
     var users = [
-        { id: 3, username: 'adron', token: '123456789', email: 'adron@deconstructed.io' },
+        { id: 2, username: 'adron', token: 'zrack123!', email: 'adron@deconstructed.io'},
+        { id: 3, username: 'public', token: '123456789', email: 'public@deconstructed.io' },
         { id: 4, username: 'aaron', token: 'abcdefghi', email: 'aaron@deconstructed.io' },
         { id: 6, username: 'consociation', token: '0d1b02f9-c7e9-42c3-8518-7d744b827274', email: 'consociation@deconstructed.io'},
         { id: 7, username: 'erskine', token: '0d1b02f9-c7e9-42c3-8518-7d744b827274', email: 'erskine@someplace.com'},
