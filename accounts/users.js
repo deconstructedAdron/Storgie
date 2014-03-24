5274fa4e3f8d8c5bd2d7e94e47e20df12e46f4f9
Adron Hall <adronhall@gmail.com>
2014-03-24 15:32:09 -0700

    Added another user.

--- accounts/users.js
+++ accounts/users.js
@@ -12,9 +12,10 @@ users.getUsers = function getUsers() {
         { id: 3, username: 'public', token: '123456789', email: 'public@deconstructed.io' },
         { id: 4, username: 'aaron', name: 'Aaron Gray', token: 'abcdefghi', email: 'aaron@deconstructed.io' },
         { id: 6, username: 'consociation', token: '0d1b02f9-c7e9-42c3-8518-7d744b827274', email: 'consociation@deconstructed.io'},
-        { id: 7, username: 'erskine', name: 'Erskine', token: 'd52de5ff-2cc4-4054-83d0-5bdf04311992', email: 'erskine@someplace.com'},
-        { id: 8, username: 'pete', name: 'Pete Bakke', token: 'f97dbc35-dacc-4135-a335-dc1d3029b44d', email: 'pete@someplace.com'},
+        { id: 7, username: 'erskine', name: 'Erskine', token: 'd52de5ff-2cc4-4054-83d0-5bdf04311992', email: 'erskine_williams@mac.com'},
+        { id: 8, username: 'peat', name: 'Peat Bakke', token: 'f97dbc35-dacc-4135-a335-dc1d3029b44d', email: 'peat@peat.org'},
         { id: 9, username: 'darin', name: 'Darin Glatt', token: '6dd85319-3e00-4ee8-b648-fcc7a2e1b56f', email: 'dglatt@gmail.com'}
+
     ];
 
     return users;
