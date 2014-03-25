65080c243c2c7b83dda5dca8248df90065b0a5b4
Adron Hall <adronhall@gmail.com>
2014-03-24 22:09:07 -0700

    Refactoring and increasing the number of tests displayed.

--- tests/_accounts_users.js
+++ tests/_accounts_users.js
@@ -7,29 +7,8 @@
 var assert = require("assert")
 
 describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
-        })
+    it('should make tests that actually matter...', function () {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
     })
 })
 
-var android_generator = require('../generators/android');
-var ios_generator = require('../generators/ios');
-
-describe('generator', function () {
-    describe('plural', function () {
-        it('should return multiple generated android idents', function () {
-            assert.equal(android_generator.idents_generate(10).length, 10);
-        })
-    })
-})
-
-describe('generator', function () {
-    describe('plural', function () {
-        it('should return multiple generated iOS idents', function () {
-            assert.equal(ios_generator.idents_generate(8).length, 8);
-        })
-    })
-})
