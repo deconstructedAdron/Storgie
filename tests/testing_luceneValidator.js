76c24f0d76f7f07555edb0723ffdf68d2831a71d
Adron Hall <adronhall@gmail.com>
2014-06-10 16:52:55 -0700

    Adding the testing_luceneValidator.js file for tests.

---
+++ tests/testing_luceneValidator.js
@@ -0,0 +1,7 @@
+/**
+ * Created by adronhall on 6/10/14.
+ * Description: Testing lucene validation calls.
+ */
+
+var should = require ("should");
+var Consociator = require ('../consociations/consociator');
