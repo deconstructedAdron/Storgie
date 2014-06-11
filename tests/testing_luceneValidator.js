becb0b5c22caaf72462967d82da7473993e614b1
Adron Hall <adronhall@gmail.com>
2014-06-10 17:06:03 -0700

    Added tests for lucene validator around escape characters and valid characters - that are of course groovy or funky.

--- tests/testing_luceneValidator.js
+++ tests/testing_luceneValidator.js
@@ -4,4 +4,31 @@
  */
 
 var should = require ("should");
-var Consociator = require ('../consociations/consociator');
+var luceneValidator = require ('../Lucene/luceneValidator');
+
+describe ('Lucene Validator', function () {
+    it ('should remove multiple escapes', function () {
+        var query = "\\* foo \\haha";
+        luceneValidator.removeEscapes (query).should.eql (" foo aha");
+    });
+
+    it ('should remove forward escapes', function () {
+        var query = "\\\\foo";
+        luceneValidator.removeEscapes (query).should.eql ("foo");
+    });
+
+    it ('should remove reverse escapes', function () {
+        var query = "foo\\\"";
+        luceneValidator.removeEscapes (query).should.eql ("foo");
+    });
+
+    it ('should check for allowed characters that are funky', function () {
+        var query = "a-zA-Z0-9_+-:.()\"*?&|!{}[]^~\\@#/$%'= ";
+        (luceneValidator.checkAllowedCharacters (query) === undefined).should.be.true;
+    });
+
+    it ('should check for allowed characters that are groovy', function () {
+        var query = "foobar";
+        (luceneValidator.checkAllowedCharacters (query) === undefined).should.be.false;
+    });
+});
